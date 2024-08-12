a2c72caa27eda77421e2936a12f02bf3681975f1
dbasu03 <116761602+dbasu03@users.noreply.github.com>
2024-08-13 02:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
 import streamlit as st
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-
+import pandas as pd
+import numpy as np
